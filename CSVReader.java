143e5cbc3cad47254226101f881c432a40453bc0
DylanLambert502 <75022302+DylanLambert502@users.noreply.github.com>
2020-12-06 01:29:49 +0000

    Update CSVReader.java
    Added option to search by routing key

--- CSVReader.java
+++ CSVReader.java
@@ -6,7 +6,7 @@ public class CSVReader {
     public static final String delimiter = ",";
 
 
-    public static void read(String csvFile){
+    public static void read(String csvFile) throws IOException {
         try {
             File file = new File(csvFile);
             FileReader fr = new FileReader(file);
@@ -27,7 +27,8 @@ public class CSVReader {
                         System.out.print(tempArr[1] +" "+ tempArr[6] +" "+ tempArr[7] +" "+ tempArr[8] + "\n");
                     }
                 }
-            } else if (optionChoice.equals("2")) {
+            }
+            else if (optionChoice.equals("2")) {
                 System.out.println("Owner Name: ");
                 String name = keyboard.nextLine();
                 String[] tempArr;
@@ -39,13 +40,32 @@ public class CSVReader {
                 }
 
             }
+            else if(optionChoice.equals("4")) {
+                System.out.println("Routing Key: ");
+                String rKey = keyboard.nextLine();
+                String[] tempArr;
+                double totalTax = 0;
+                int numProp = 0;
+                while ((line = br.readLine()) != null) {
+                    tempArr = line.split(delimiter);
+                    if (tempArr[2].startsWith(rKey)) {
+                        //Total Tax paid
+                        totalTax += Double.valueOf(tempArr[6]);
+                        //Average Tax Paid
+                        numProp++;
+                    }
+                }
+                System.out.println("Total tax paid: "+ totalTax);
+                System.out.println("Average tax paid per property: "+ Math.round(totalTax/numProp));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-         //Test for file read
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         // csv file to read
         String csvFile = "/home/default/Documents/cs4013/TaxSolutionsIreland/AllProperties.csv";
         CSVReader.read(csvFile);
