047c62072822452df77d89cce06f9aae56c0e6a7
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-09 23:06:20 +0000

    Update DeptOfEnvironmentMenu.java

--- DeptOfEnvironmentMenu.java
+++ DeptOfEnvironmentMenu.java
@@ -17,7 +17,8 @@ public class DeptOfEnvironmentMenu {
         ReadFromFileMethods readFromFileMethods = new ReadFromFileMethods();
 
         while(more){
-            System.out.println("1) Payment Data for a Property, 2) Payments made by an Owner, 3) All Properties with OverDue Tax\n");
+            System.out.println("1) Payment Data for a Property, 2) Payments made by an Owner, 3) All Properties with OverDue Tax\n" +
+                    "4) Get Property Tax Statistics");
             String command = keyboard.nextLine().toUpperCase();
 
             if (command.equalsIgnoreCase("1")) {
@@ -29,7 +30,7 @@ public class DeptOfEnvironmentMenu {
 
             else if( command.equalsIgnoreCase("2")){
                 System.out.print( readFromFileMethods.getOwnerNames() );
-                System.out.println("Choose the owner whos payments you would like to view (eg. 'David McCarthy'): ");
+                System.out.println("Choose the owner whos payments you would like to view: ");
                 System.out.println("BEWARE, THIS IS CASE SENSITIVE");
                 String choice = keyboard.nextLine();
                 System.out.println(readFromFileMethods.getPaymentsForAnOwner(choice));
@@ -46,6 +47,14 @@ public class DeptOfEnvironmentMenu {
                     choice = keyboard.nextLine();
                     System.out.print( readFromFileMethods.getOverDueTaxWithinAnArea(choice) );
                 }
+            }
+
+            else if(command.equalsIgnoreCase("4")){
+                System.out.println( readFromFileMethods.getAllAreaCodes());
+                System.out.println( "Choose an Area Code from List Above: ");
+                String choice = keyboard.nextLine();
+                System.out.print( readFromFileMethods.getTaxStatisticsWithinAnArea(choice) );
+
             }
         }
     }
