aa61d6ceef08bacdcdb78a5a27da794907a5a5f1
TaxiDave8 <75022309+TaxiDave8@users.noreply.github.com>
2020-12-09 19:36:29 +0000

    Update ManagementSystemSim.java
    Now it will either run the GUI or the CLI depending on user input

--- ManagementSystemSim.java
+++ ManagementSystemSim.java
@@ -1,19 +1,31 @@
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
+/**
+ * The class with the main method which the program will be ran from
+ */
 public class ManagementSystemSim {
 
-    public static void main(String[] args ){
-        Scanner keyboard = new Scanner( System.in);
+    public static void main(String[] args ) throws IOException {
+        Scanner keyboard = new Scanner( System.in );
         PropertyOwnerMenu ownerMenu = new PropertyOwnerMenu();
         DeptOfEnvironmentMenu deptMenu = new DeptOfEnvironmentMenu();
 
-        System.out.println("Are you a 1) Property Owner or a 2) Dept of Environment Manager");
-        int menuChoice = keyboard.nextInt();
-        if( menuChoice == 1){
-            ownerMenu.run();
-        } else if (menuChoice == 2){
-            deptMenu.run();
+        System.out.println(" Would you like to use our 1) Command Line Interface or 2) GUI");
+        int interfaceChoice = keyboard.nextInt();
+        if (interfaceChoice == 1) {
+            System.out.println("Are you a 1) Property Owner or a 2) Dept of Environment Manager");
+            int menuChoice = keyboard.nextInt();
+            if (menuChoice == 1) {
+                ownerMenu.run();
+            } else if (menuChoice == 2) {
+                deptMenu.run();
+            }
+        } else if( interfaceChoice == 2){
+                GUI.main(args);
+            }
         }
-    }
 }
